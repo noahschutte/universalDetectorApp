6ad133ab922f81a99b0a386a5e72a5172e27af0a
Noah Schutte <noahschutte@gmail.com>
2017-11-11 23:54:10 -0700

    initial commit

--- App.js
+++ App.js
@@ -1,37 +1,62 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
 
 import React, { Component } from 'react';
 import {
-  Platform,
   StyleSheet,
   Text,
-  View
+  View,
+  Slider,
+  Dimensions
 } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import Sound from 'react-native-sound';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
+const beep = new Sound('beep6.mp3', Sound.MAIN_BUNDLE, (error) => {
+  if (error) {
+    // console.log('failed to load the sound', error);
+    return;
+  }
 });
 
 export default class App extends Component<{}> {
+  play = () => {
+    beep.play(success => {
+      if (success) {
+        // console.log('successfully finished playing');
+      } else {
+        // console.log('playback failed due to audio decoding errors');
+        beep.reset();
+      }
+    })
+  }
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+
+        <View style={styles.settingsBar}>
+          <Icon.Button
+            name='cog'
+            color='grey'
+            backgroundColor='rgba(0,0,0,0)'
+            size={25}
+            onPress={null}
+          />
+        </View>
+
+        <View style={styles.detectorTextWrapper}>
+          <Text style={styles.detectorText}>
+            Universal Detector!
+          </Text>
+        </View>
+
+        <View style={styles.sliderWrapper}>
+          <Slider
+            style={styles.slider}
+            minimumValue={0.5}
+            maximumValue={100}
+            onValueChange={this.play}
+          />
+        </View>
+
       </View>
     );
   }
@@ -44,14 +69,24 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
+  detectorText: {
     fontSize: 20,
     textAlign: 'center',
     margin: 10,
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  detectorTextWrapper: {
+    flex: 7,
+    justifyContent: 'center',
+  },
+  settingsBar: {
+    flex: 1,
+    alignSelf: 'flex-end',
+    margin: 5,
+  },
+  sliderWrapper: {
+    flex: 1,
+  },
+  slider: {
+    width: Dimensions.get('window').width,
   },
 });
